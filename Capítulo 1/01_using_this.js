4a504fc4db0eeb7b131ff9895d629b778b3ff641
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-06-14 23:25:59 -0300

    Adiciona exemplo 'this'

---
+++ Capítulo 1/01_using_this.js
@@ -0,0 +1,12 @@
+// Teste para uso de função com this
+
+function Point(x, y) {
+  // Segundo o próprio livro, a palavra "this" significa a criação de um novo objeto!
+  this.x = x;
+  this.y = y;
+
+  // Testando com o console.log(), é possivel constatar o retorno de tal objeto.
+  console.log(this);
+}
+
+Point('teste1', 'teste2');
